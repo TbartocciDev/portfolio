8e4328dee50b104fefe4dd14a1623189f99b0060
TbartocciDev <tommybartocci@gmail.com>
2023-06-24 18:10:07 -0400

    base formatting tweaks

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,10 @@
 import "./ContactPage.css"
+import TestSection from "./TestSection"
 
 export default function ContactPage({ data }) {
     return (
         <main className="ContactPage">
-            
+            <TestSection />
         </main>
     )
 }
---
+++ src/Pages/ContactPage/TestSection.jsx
@@ -0,0 +1,15 @@
+import "./Test.css"
+import ProjectCard from "../HomePage/ProjectsSection/Components/ProjectCard/ProjectCard"
+
+export default function TestSection() {
+    return (
+        <main className="TestSection">
+            <div className="content">
+                <h1>Test Section</h1>
+                <div className="test-holder">
+                    
+                </div>
+            </div>
+        </main>
+    )
+}
