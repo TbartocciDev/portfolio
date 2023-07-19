cf60416033201536bc23e5a0c62547414400b779
TbartocciDev <tommybartocci@gmail.com>
2023-07-18 23:10:40 -0400

    blog is latest 3 posts

--- src/Pages/HomePage/RecentBlogSection/RecentBlogSection.jsx
+++ src/Pages/HomePage/RecentBlogSection/RecentBlogSection.jsx
@@ -2,12 +2,15 @@ import "./RecentBlogSection.css"
 import BlogPreview
  from "./BlogPreview/BlogPreview"
 export default function RecentBlogSection({ data }) {
+    const latestBlogs = data.blogs.slice(0,3)
+    
     return (
         <main className="RecentBlogSection">
             <div className="content">
                 <h1>Recent Blog Posts</h1>
+                <h2>Dive into what I've been up to lately</h2>
                 <div className="blogs">
-                    {data.blogs.map((blog, key) => (
+                    {latestBlogs.map((blog, key) => (
                         <BlogPreview data={blog} color={data.colors[key]} key={key}/>
                     ))}
                 </div>
