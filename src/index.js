f4de03a2c19925c9c9ad2995f4fa9bb81232c1f6
TbartocciDev <tommybartocci@gmail.com>
2023-07-23 21:28:41 -0400

    package upadted

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
